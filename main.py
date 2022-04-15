ffad5ceb01cc48945e7a839976520215f7931d23
Ходоров <ivan.lap008@mail.ru>
2022-04-15 20:11:16 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,58 @@
+from config import open_weather_token
+from pprint import pprint
+import datetime
+import requests
+
+def get_weather(city, open_weather_token):
+
+    code_to_smile = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026A1",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туман \U0001F32B"
+    }
+
+    try:
+        r = requests.get(
+            f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric'
+        )
+        data = r.json()
+        pprint(data)
+
+        cur_weather = data['main']['temp']
+
+        weather_descriptioon = data['weather'][0]['main']
+        if weather_descriptioon in code_to_smile:
+            wd = code_to_smile[weather_descriptioon]
+        else:
+            wd = 'Посмотри в окно, не пойму что там за погода!'
+
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+            data["sys"]["sunrise"])
+
+        print(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
+              f"Погода в городе: {city}\nТемпература: {cur_weather}C° {wd}\n"
+              f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
+              f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
+              f"Хорошего дня!"
+              )
+    except Exception as ex:
+        print(ex)
+        print('Проверьте название города')
+
+
+def main():
+    city = input('Введите город: ')
+    get_weather(city, open_weather_token)
+
+
+if __name__ == '__main__':
+    main()
